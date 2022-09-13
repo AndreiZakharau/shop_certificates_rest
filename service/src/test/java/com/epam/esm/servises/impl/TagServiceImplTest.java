72abe4baf11e115b32f93bd3a366f259aadaf49c
Andrei Zakharau1 <andrei_zakharau1@epam.com>
2022-09-14 00:15:11 +0300

    update test Tag

--- service/src/test/java/com/epam/esm/servises/impl/TagServiceImplTest.java
+++ service/src/test/java/com/epam/esm/servises/impl/TagServiceImplTest.java
@@ -1,82 +1,120 @@
-//package com.epam.esm.servises.impl;
-//
-//import com.epam.esm.mapper.TagDto;
-//import com.epam.esm.entitys.Tag;
-//import com.epam.esm.repositorys.impl.TagRepositoryImpl;
-//import com.epam.esm.util.impl.TagsValidator;
-//import lombok.RequiredArgsConstructor;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//
-//import java.util.Optional;
-//
-//@RequiredArgsConstructor
-//class TagServiceImplTest {
-//
-//    private TagServiceImpl tagServiceImpl;
-//    private final TagRepositoryImpl mockTagRepositoryImpl = Mockito.mock(TagRepositoryImpl.class);
-//    private final TagsValidator mockTagValid = Mockito.mock(TagsValidator.class);
-//    private final CertificateServiceImpl mockCertificateServiceImpl = Mockito.mock(CertificateServiceImpl.class);
-//    private final TagDto mockTagDto = Mockito.mock(TagDto.class);
-//
-//    @BeforeEach
-//    public void setMoc(){
-//        tagServiceImpl = new TagServiceImpl(mockTagRepositoryImpl,mockTagValid, mockCertificateServiceImpl,mockTagDto);
-//    }
-//
-//    @AfterEach
-//    public void afterMetods(){
-//        tagServiceImpl = null;
-//    }
-//
-//    @Test
-//    void getAllEntity() {
-////        Mockito.when(mockTagRepositoryImpl.getAllEntity()).thenReturn( List.of(new Tag(1L,"sale"),
-////                new Tag(2L,"free"),new Tag(3L,"10%")));
-////        Page<Tag> list = tagServiceImpl.getAllEntity();
-////        Assertions.assertNotNull(list);
-////        Mockito.verify(mockTagRepositoryImpl).getAllEntity();
-////        Assertions.assertEquals(list.getTotalElements(),2);
-//    }
-//
-//    @Test
-//    void saveEntity() {
-//        Tag tag = new Tag(13L,"ok");
-//        Mockito.doNothing().when(mockTagRepositoryImpl).addEntity(tag);
-//        Mockito.when(mockTagValid.isValid(tag)).thenReturn(true);
-//        Assertions.assertEquals(tag.getId(),13L);
-//        Assertions.assertEquals(tag.getTagName(),"ok");
-//    }
-//
-//    @Test
-//    void updateEntity() {
-//        Tag tag = new Tag(13L,"ok");
-//        Assertions.assertEquals(tag.getTagName(), "ok");
-//        String name = "double";
-//        tag.setTagName(name);
-//        Mockito.doNothing().when(mockTagRepositoryImpl).addEntity(tag);
-//        tagServiceImpl.updateEntity(13L, tag);
-//        Assertions.assertEquals(tag.getTagName(), name);
-//    }
-//
-//    @Test
-//    void getEntity() {
-//        Tag tag = new Tag(3L,"sale");
-//        Mockito.when(mockTagRepositoryImpl.getEntity(3)).thenReturn(java.util.Optional.of(tag));
-//        Optional<Tag> t = tagServiceImpl.getEntity(3);
-//        Assertions.assertNotNull(t);
-//        Mockito.verify(mockTagRepositoryImpl).getEntity(Mockito.anyLong());
-//    }
-//
-//    @Test
-//    void deleteEntity() {
-//        Tag tag = new Tag(3L,"sale");
-//        Mockito.when(mockTagRepositoryImpl.getEntity(3)).thenReturn(Optional.of(tag));
-//        tagServiceImpl.deleteEntity(3);
-//        Mockito.verify(mockTagRepositoryImpl).deleteEntity(Mockito.anyLong());
-//        Mockito.when(mockTagRepositoryImpl.getEntity(3)).thenReturn(null);
-//    }
-//}
+package com.epam.esm.servises.impl;
+
+
+import com.epam.esm.entitys.Tag;
+import com.epam.esm.mapper.impl.tagMapper.CreateTagFromTagModelMapper;
+import com.epam.esm.mapper.impl.tagMapper.OnlyTagReadMapper;
+import com.epam.esm.mapper.impl.tagMapper.TagModelReadMapper;
+import com.epam.esm.models.tags.TagModel;
+import com.epam.esm.repositorys.impl.TagRepositoryImpl;
+import com.epam.esm.util.impl.TagsValidator;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+class TagServiceImplTest {
+
+    private TagServiceImpl tagServiceImpl;
+    private final TagRepositoryImpl mockTagRepositoryImpl = Mockito.mock(TagRepositoryImpl.class);
+    private final TagsValidator mockTagValid = Mockito.mock(TagsValidator.class);
+    private final CertificateServiceImpl mockCertificateServiceImpl = Mockito.mock(CertificateServiceImpl.class);
+    private final CreateTagFromTagModelMapper mockCreateTag = Mockito.mock(CreateTagFromTagModelMapper.class);
+    private final TagModelReadMapper mockTagModel = Mockito.mock(TagModelReadMapper.class);
+    private final OnlyTagReadMapper mockOnlyTag = Mockito.mock(OnlyTagReadMapper.class);
+
+    private final static Tag TAG = Tag.builder()
+            .id(13L)
+            .tagName("coffee")
+            .build();
+    private final static Tag TAG2 = Tag.builder()
+            .id(11L)
+            .tagName("sale")
+            .build();
+
+
+    @BeforeEach
+    public void setMoc(){
+        tagServiceImpl = new TagServiceImpl(mockTagRepositoryImpl,mockTagValid, mockCertificateServiceImpl,mockCreateTag,mockTagModel,mockOnlyTag);
+    }
+
+    @AfterEach
+    public void afterMetods(){
+        tagServiceImpl = null;
+    }
+
+    @Test
+    void getAllEntity() {
+        int limit=1;
+        int offset=2;
+        Mockito.when(mockTagRepositoryImpl.getAllEntity(limit, offset)).thenReturn( List.of(TAG,TAG2));
+        List<Tag> list = mockTagRepositoryImpl.getAllEntity(limit, offset);
+        Assertions.assertNotNull(list);
+        Mockito.verify(mockTagRepositoryImpl).getAllEntity(limit, offset);
+        Assertions.assertEquals(list.size(),2);
+    }
+
+    @Test
+    void saveEntity() {
+        Tag tag = TAG2;
+        Mockito.doNothing().when(mockTagRepositoryImpl).addEntity(tag);
+        Mockito.when(mockTagValid.isValid(tag)).thenReturn(true);
+        Assertions.assertEquals(tag.getId(),11L);
+        Assertions.assertEquals(tag.getTagName(),"sale");
+    }
+
+    @Test
+    void updateEntity() {
+        Tag tag = TAG;
+        Assertions.assertEquals(tag.getTagName(), "coffee");
+        String name = "free";
+        tag.setTagName(name);
+        Mockito.doNothing().when(mockTagRepositoryImpl).addEntity(tag);
+        tagServiceImpl.updateEntity(13L, mockTagModel.mapFrom(tag));
+        Assertions.assertEquals((tag.getTagName()),name );
+    }
+
+    @Test
+    void findById() {
+        Mockito.when(mockTagRepositoryImpl.getEntityById(13)).thenReturn(java.util.Optional.of(TAG));
+        Optional<TagModel> t = tagServiceImpl.findById(13);
+        Assertions.assertNotNull(t);
+        Mockito.verify(mockTagRepositoryImpl).getEntityById(Mockito.anyLong());
+    }
+
+    @Test
+    void deleteEntity() {
+        Mockito.when(mockTagRepositoryImpl.getEntityById(13L)).thenReturn(Optional.of(TAG));
+        tagServiceImpl.deleteEntity(13);
+        Mockito.verify(mockTagRepositoryImpl).deleteEntity(Mockito.anyLong());
+        Mockito.when(mockTagRepositoryImpl.getEntityById(13)).thenReturn(null);
+    }
+
+    @Test
+    void listOnlyTags() {
+        List<Tag> tags = List.of(TAG,TAG2);
+        Mockito.when(mockTagRepositoryImpl.getOnlyTags()).thenReturn(tags);
+        List<Tag> list = mockTagRepositoryImpl.getOnlyTags();
+        Mockito.verify(mockTagRepositoryImpl).getOnlyTags();
+        Assertions.assertNotNull(list);
+        Assertions.assertEquals(list.size(),2);
+
+    }
+
+
+
+    @Test
+    void countAllTags() {
+        List<Tag> tags = List.of(TAG,TAG2);
+        Mockito.when(mockTagRepositoryImpl.countAllTags()).thenReturn(tags.size());
+        int counts = tagServiceImpl.countAllTags();
+        Mockito.verify(mockTagRepositoryImpl).countAllTags();
+        Assertions.assertEquals(counts,2);
+    }
+}
